88b31746586b5adfd19f090bbe4fdc51fdf81226
ShinhyoBelok <shibo6937@gmail.com>
2023-03-29 19:10:52 -0400

    Add most recent comments and update methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,19 @@
 class Post < ApplicationRecord
-    has_many :comments
-    has_many :likes
-    belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :comments, foreign_key: :posts_id
+  has_many :likes, foreign_key: :posts_id
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+
+  def most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  def update_comment_counter
+    self.comments_counter = comments.count
+    save
+  end
+
+  def update_like_counter
+    self.likes_counter = likes.count
+    save
+  end
 end
