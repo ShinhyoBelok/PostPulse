ffef668575936201e5d3277a9c10cc454e55edaa
ShinhyoBelok <shibo6937@gmail.com>
2023-03-29 19:11:12 -0400

    Add most recent post and update method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
-    has_many :posts
-    has_many :likes
-    has_many :comments
+  has_many :posts, class_name: 'Post', foreign_key: :author_id
+  has_many :likes, foreign_key: :users_id
+  has_many :comments
+
+  def most_recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
+
+  def update_post_counter
+    self.posts_counter = posts.count
+    save
+  end
 end
