6449a316a3b9b8410975f94e58a245fb65b14e06
ShinhyoBelok <shibo6937@gmail.com>
2023-03-30 19:45:54 -0400

    Fix migration files

--- db/migrate/20230330132351_rename_users_id_for_author_id.rb
+++
@@ -1,6 +0,0 @@
-class RenameUsersIdForAuthorId < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :comments, :users_id, :author_id
-    rename_column :likes, :users_id, :author_id
-  end
-end
