4e457171898ff437b1d08f6453710eb4c31c7e8c
ShinhyoBelok <shibo6937@gmail.com>
2023-04-04 16:01:47 -0400

    Add render correct template test for users

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,7 +7,12 @@ RSpec.describe UsersController, type: :request do
       expect(response).to have_http_status(200)
     end
 
-    it "displays the correct template and content" do
+    it "displays the correct template" do
+      get "/users"
+      expect(response).to render_template(:index)
+    end
+
+    it "displays the correct content" do
       get "/users"
       expect(response.body).to include("List of user name and the number of posts")
     end
@@ -19,7 +24,12 @@ RSpec.describe UsersController, type: :request do
       expect(response).to have_http_status(200)
     end
 
-    it "displays the correct template and content" do
+    it "displays the correct template" do
+      get "/users/:id"
+      expect(response).to render_template(:show)
+    end
+
+    it "displays the correct content" do
       get "/users/1"
       expect(response.body).to include("Show user information with the requested id 1")
     end
