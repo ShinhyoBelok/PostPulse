1a099a6180d2e2d554aae26142f1dd673108e034
ShinhyoBelok <shibo6937@gmail.com>
2023-04-04 16:02:37 -0400

    Fix linters

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,37 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :request do
-  describe "GET #index" do
-    it "returns a successful response" do
-      get "/users/:user_id/posts"
+  describe 'GET #index' do
+    it 'returns a successful response' do
+      get '/users/:user_id/posts'
       expect(response).to have_http_status(200)
     end
 
-    it "displays the correct template" do
-        get "/users/:user_id/posts"
-        expect(response).to render_template(:index)
+    it 'displays the correct template' do
+      get '/users/:user_id/posts'
+      expect(response).to render_template(:index)
     end
 
-    it "displays the correct content" do
-      get "/users/123/posts"
-      expect(response.body).to include("Show a list of all related posts of user 123")
+    it 'displays the correct content' do
+      get '/users/123/posts'
+      expect(response.body).to include('Show a list of all related posts of user 123')
     end
   end
 
-  describe "GET #show" do
-    it "returns a successful response" do
-      get "/users/:user_id/posts/:id"
+  describe 'GET #show' do
+    it 'returns a successful response' do
+      get '/users/:user_id/posts/:id'
       expect(response).to have_http_status(200)
     end
 
-    it "displays the correct template" do
-        get "/users/:user_id/posts/:id"
-        expect(response).to render_template(:show)
+    it 'displays the correct template' do
+      get '/users/:user_id/posts/:id'
+      expect(response).to render_template(:show)
     end
 
-    it "displays the correct content" do
-      get "/users/3/posts/44"
-      expect(response.body).to include("Show a post with id 44 of user 3")
+    it 'displays the correct content' do
+      get '/users/3/posts/44'
+      expect(response.body).to include('Show a post with id 44 of user 3')
     end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,37 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :request do
-  describe "GET #index" do
-    it "returns a successful response" do
-      get "/users"
+  describe 'GET #index' do
+    it 'returns a successful response' do
+      get '/users'
       expect(response).to have_http_status(200)
     end
 
-    it "displays the correct template" do
-      get "/users"
+    it 'displays the correct template' do
+      get '/users'
       expect(response).to render_template(:index)
     end
 
-    it "displays the correct content" do
-      get "/users"
-      expect(response.body).to include("List of user name and the number of posts")
+    it 'displays the correct content' do
+      get '/users'
+      expect(response.body).to include('List of user name and the number of posts')
     end
   end
 
-  describe "GET #show" do
-    it "returns a successful response" do
-      get "/users/:id"
+  describe 'GET #show' do
+    it 'returns a successful response' do
+      get '/users/:id'
       expect(response).to have_http_status(200)
     end
 
-    it "displays the correct template" do
-      get "/users/:id"
+    it 'displays the correct template' do
+      get '/users/:id'
       expect(response).to render_template(:show)
     end
 
-    it "displays the correct content" do
-      get "/users/1"
-      expect(response.body).to include("Show user information with the requested id 1")
+    it 'displays the correct content' do
+      get '/users/1'
+      expect(response.body).to include('Show user information with the requested id 1')
     end
   end
 end
