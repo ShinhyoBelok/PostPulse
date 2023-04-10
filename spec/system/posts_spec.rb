2b34031ec94bdb29f3c8dd7ce893b17a8804e1d4
ShinhyoBelok <shibo6937@gmail.com>
2023-04-10 19:00:51 -0400

    Add index and show testing units for post

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :system do
   before :each do
     driven_by(:rack_test)
-    @user = User.create(name: 'Tornado',
-                        photo: 'https://unsplash.com/photos/Tornado',
-                        bio: 'Complete bio Tornado',
-                        posts_counter: 0)
+    @user = User.create(
+      name: 'Tornado',
+      photo: 'https://unsplash.com/photos/Tornado',
+      bio: 'Complete bio Tornado',
+      posts_counter: 0
+    )
     @post = Post.create(
       title: 'post 1',
       text: 'my last recent post',
@@ -14,32 +16,32 @@ RSpec.describe 'Posts', type: :system do
       author: @user
     )
     Post.create(
-        title: 'post 2',
-        text: 'new post 2',
-        comments_counter: 0,
-        likes_counter: 0,
-        author: @user
-      )
-    Post.create(
-        title: 'post 3',
-        text: 'new post 3',
-        comments_counter: 0,
-        likes_counter: 0,
-        author: @user
+      title: 'post 2',
+      text: 'new post 2',
+      comments_counter: 0,
+      likes_counter: 0,
+      author: @user
     )
     Post.create(
-        title: 'post 4',
-        text: 'new post 4',
-        comments_counter: 0,
-        likes_counter: 0,
-        author: @user
+      title: 'post 3',
+      text: 'new post 3',
+      comments_counter: 0,
+      likes_counter: 0,
+      author: @user
     )
     Post.create(
-        title: 'post 5',
-        text: 'new post 5',
-        comments_counter: 0,
-        likes_counter: 0,
-        author: @user
+      title: 'post 4',
+      text: 'new post 4',
+      comments_counter: 0,
+      likes_counter: 0,
+      author: @user
+    )
+    Post.create(
+      title: 'post 5',
+      text: 'new post 5',
+      comments_counter: 0,
+      likes_counter: 0,
+      author: @user
     )
     Comment.create(author: @user, post: @post, text: 'comment 1')
     Comment.create(author: @user, post: @post, text: 'comment 2')
@@ -49,7 +51,8 @@ RSpec.describe 'Posts', type: :system do
     Comment.create(author: @user, post: @post, text: 'comment 6')
     @post1_url = "#{users_path}/#{@user.id}/posts/#{@post.id}"
   end
-  describe 'index' do
+
+  describe 'Index' do
     before :each do
       visit user_posts_path(@user)
     end
@@ -98,6 +101,53 @@ RSpec.describe 'Posts', type: :system do
     scenario 'When I click on a post, it redirects me to that posts show page.' do
       click_link 'post 1'
       expect(current_path) == @post1_url
+    end
+  end
+
+  describe 'Show' do
+    before :each do
+      visit user_post_path(@user, @post)
+    end
+
+    scenario 'User can see the first comments on a post' do
+      expect(page).to have_content('comment 6')
+      expect(page).to have_content('comment 5')
+      expect(page).to have_content('comment 4')
+      expect(page).to have_content('comment 3')
+      expect(page).to have_content('comment 2')
+      expect(page).to have_content('comment 1')
+    end
+
+    scenario 'User can see the post title' do
+      expect(page).to have_content(@post.title)
+    end
+
+    scenario 'User can see who wrote the post' do
+      expect(page).to have_content(@post.author.name)
+    end
+
+    scenario 'User can see how many comments it has' do
+      expect(page).to have_content("Comments: 6")
+    end
+
+    scenario 'User can see how many likes it has' do
+      expect(page).to have_content("Likes: 0")
+    end
+
+    scenario 'User can see the post body' do
+      expect(page).to have_content(@post.text)
+    end
+
+    scenario 'User can see the username of each commentor' do
+      @post.comments.each do |comment|
+        expect(page).to have_content(comment.author.name)
+      end
+    end
+
+    scenario 'User can see the comment each commentor left' do
+      @post.comments.each do |comment|
+        expect(page).to have_content(comment.text)
+      end
     end
   end
 end
