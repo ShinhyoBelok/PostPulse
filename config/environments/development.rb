89041efabccbb4fd28bbaaf2bd52de51ac3a2fd0
sambeck87 <sandro_mipb@hotmail.com>
2023-04-11 16:31:19 -0600

    Add email confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,23 +33,19 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
   # email config
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 465,
-    domain:               'gmail.com',
-    user_name:            ENV['ror.app.email.dev@gmail.com'],
-    password:             ENV['shbmqqaygioxxfgg'],
-    authentication:       'plain',
-    enable_starttls_auto: true,
-    :ssl => true,
-    :tsl => true,
-    enable_starttls_auto: true
-  }
+  address:              'smtp.office365.com',
+  port:                 587,
+  user_name:            'microvers_tests@hotmail.com',
+  password:             'password448819',
+  authentication:       'login',
+  enable_starttls_auto: true
+}
 
   # email confirmation
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
