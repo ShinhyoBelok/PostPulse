0e2cc3ca525331cae5a94358eda957f5c4cb803c
ShinhyoBelok <shibo6937@gmail.com>
2023-04-11 16:03:40 -0400

    Add default value attribute of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :posts, class_name: 'Post', foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
 
   validates :name, presence: true
   validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }
+
+  attribute :posts_counter, :integer, default: 0
+  attribute :photo, default: 'https://cdn3.iconfinder.com/data/icons/avatars-flat/33/man_5-512.png'
 
   def most_recent_posts
     posts.order(created_at: :desc).limit(3)
