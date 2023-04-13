5d6e1545f9f8ab3e345347926d0d30b1197146c3
sambeck87 <sandro_mipb@hotmail.com>
2023-04-13 12:49:36 -0600

    Add api application controller

---
+++ app/controllers/api/application_controller.rb
@@ -0,0 +1,19 @@
+class ApplicationController < ActionController::API
+    before_action :authenticate_user!
+    private
+    def authenticate_user!
+        authorization_header = request.headers['Authorization']
+        if authorization_header
+        token = authorization_header.split(' ').last
+        decoded_token = JWT.decode(token, Rails.application.secrets.secret_key_base, true, { algorithm: 'HS256' })
+        user_id = decoded_token[0]['user_id']
+        @current_user = User.find_by(id: user_id)
+        render json: { error: 'Unauthorized' }, status: :unauthorized unless @current_user
+        else
+        render json: { error: 'Unauthorized' }, status: :unauthorized
+        end
+    end
+    def current_user
+        @current_user
+    end
+end
