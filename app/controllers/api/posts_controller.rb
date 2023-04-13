c762f60776c022b66f948b6cdc413abc84493458
sambeck87 <sandro_mipb@hotmail.com>
2023-04-13 12:51:59 -0600

    Fix api post controler linters

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,7 +1,7 @@
 class Api::PostsController < ApplicationController
-    def api_index
-        user = User.includes(posts: :comments).find(params[:user_id])
-        posts = user.posts.includes(:comments)
-        render json: posts
-    end
+  def api_index
+    user = User.includes(posts: :comments).find(params[:user_id])
+    posts = user.posts.includes(:comments)
+    render json: posts
+  end
 end
