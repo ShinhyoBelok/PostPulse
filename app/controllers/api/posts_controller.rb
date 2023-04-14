7f1cb354e92e87425ed6b93bc524cb441de53419
ShinhyoBelok <shibo6937@gmail.com>
2023-04-14 15:27:40 -0400

    Add error handlers api reques of posts

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,7 +1,17 @@
 class Api::PostsController < Api::ApplicationController
   def api_index
-    user = User.includes(posts: :comments).find(params[:user_id])
-    posts = user.posts.includes(:comments)
-    render json: posts
+    if (user = User.includes(posts: :comments).find_by(id: params[:user_id]))
+      posts = user.posts.includes(:comments)
+    end
+
+    if user
+      if posts
+        render json: posts
+      else
+        render json: { error: 'No posts found', status: 404 }
+      end
+    else
+      render json: { error: 'No user found', status: 404 }
+    end
   end
 end
