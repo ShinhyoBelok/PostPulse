ccf243efbd7298a5dcae9d2f9e2a0f44df47a123
sambeck87 <sandro_mipb@hotmail.com>
2023-04-14 12:26:22 -0600

    Setup swagger

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "users#index"
@@ -11,7 +13,7 @@ Rails.application.routes.draw do
   # api ---
   get "/api/users/:user_id/posts", to: "api/posts#api_index"
   get "/api/users/:user_id/posts/:post_id/comments", to: "api/comments#api_index"
-  post "/api/:author_id/users/:user_id/posts/:post_id/comments", to: "api/comments#api_create_comment"
+  post "/api/users/:user_id/posts/:post_id/comments/:author_id", to: "api/comments#api_create_comment"
   # api ---
   get "/users/:id", to: "users#show"
   get "/users/:user_id/posts", to: "posts#index"
---
+++ spec/requests/api/post_spec.rb
@@ -0,0 +1,4 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/post', type: :request do
+end
