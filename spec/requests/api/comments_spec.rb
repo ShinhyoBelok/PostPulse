9d93eadd3a5d6cb25e1fc73a88be57103ef7d5b4
sambeck87 <sandro_mipb@hotmail.com>
2023-04-14 12:27:17 -0600

    Create spec and swagger.yaml file

---
+++ spec/requests/api/comments_spec.rb
@@ -0,0 +1,56 @@
+require 'swagger_helper'
+
+describe 'Comments API' do
+  path '/api/users/1/posts/1/comments' do
+    get 'Retrieves all comments for a post' do
+      tags 'Comments'
+      produces 'application/json'
+      parameter name: :post_id, in: :query, type: :integer
+
+      response '200', 'comments found' do
+        let(:post) { create(:post) }
+        let(:post_id) { post.id }
+
+        run_test!
+      end
+
+      response '404', 'post not found' do
+        let(:post_id) { 'invalid' }
+
+        run_test!
+      end
+    end
+  end
+
+  path '/api/users/1/posts/1/comments' do
+    post 'Creates a comment for a post' do
+      tags 'Comments'
+      consumes 'application/json'
+      parameter name: :comment, in: :body, schema: {
+        type: :object,
+        properties: {
+          text: { type: :string },
+        },
+        required: %w[text user_id post_id author_id]
+      }
+
+      response '201', 'comment created' do
+        let(:user) { create(:user) }
+        let(:post) { create(:post, user: user) }
+        let(:comment) { build(:comment, user: user, post: post) }
+        let(:comment) { { text: comment.text, user_id: comment.user.id, post_id: comment.post.id, author_id: comment.author.id } }
+
+        run_test!
+      end
+
+      response '422', 'invalid request' do
+        let(:user) { create(:user) }
+        let(:post) { create(:post, user: user) }
+        let(:comment) { build(:comment, user: user, post: post) }
+        let(:comment) { { text: '', user_id: comment.user.id, post_id: comment.post.id, author_id: comment.author.id } }
+
+        run_test!
+      end
+    end
+  end
+end
